d24841dd61f10d441fc413d0b21a08f51bfe12ef
chaueq <chaueq@protonmail.com>
2024-12-02 16:33:13 +0100

    Added content types for served files

--- src/http_handler.rs
+++ src/http_handler.rs
@@ -1,4 +1,4 @@
-use std::{fs::{File, OpenOptions}, io::{ Read, Write}, os::unix::fs::chown};
+use std::{collections::HashMap, fs::{File, OpenOptions}, io::{ Read, Write}, os::unix::fs::chown};
 
 use serde_json::json;
 
@@ -22,11 +22,23 @@ pub fn serve_file(req: HttpRequest, config: &Config) -> HttpResponse {
     } {
         Some(f) => {
             let path = config.get_path(f);
+            let mut headers: HashMap<String, String> = HashMap::new();
+            headers.insert("content-type".to_string(), (
+                    if path.ends_with(".html") {"text/html"}
+                    else if path.ends_with(".css") {"text/css"}
+                    else if path.ends_with(".js") {"application/javascript"}
+                    else {"text/plain"}
+                ).to_string() + "; charset=utf-8"
+            );
             match File::open(path) {
                 Ok(mut file) => {
                     let mut content = String::new();
                     match file.read_to_string(&mut content) {
-                        Ok(_) => {HttpResponse::normal(content)}
+                        Ok(_) => {HttpResponse {
+                            status: 200,
+                            headers,
+                            body: Some(content),
+                        }}
                         Err(_) => {HttpResponse::minimal(500)}
                     }
                     
